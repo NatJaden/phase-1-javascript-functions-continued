9434e456111172ae1872eea20883e8ac61a3dcd2
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-03-21 09:08:23 +0300

    Functions: Continued

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function saturdayFun(activity="roller-skate") {
-    return `This Saturday, I want to ${activity}!`
+function saturdayFun(activity = "roller-skate" || "bathe my dog") {
+  return `This Saturday, I want to ${activity}!`;
 }
 
-function mondayWork(activity="go to the office") {
-    return `This Monday, I will ${activity}.`
+function mondayWork(activity = "go to the office" || "I will work from home") {
+  return `This Monday, I will ${activity}.`;
 }
 
-function wrapAdjective(result="*") {
-    return function(emphatic="a hard worker") {
-        return `You are ${result}${emphatic}${result}!`
-    }
+function wrapAdjective(result = "*") {
+  return function (emphatic = "a hard worker") {
+    return `You are ${result}${emphatic}${result}!`;
+  };
 }
-function wrapAdjective(result="||") {
-    return function(emphatic="a hard worker") {
-        return `You are ${result}${emphatic}${result}!`
-    }
+function wrapAdjective(result = "||") {
+  return function (emphatic = "a hard worker") {
+    return `You are ${result}${emphatic}${result}!`;
+  };
 }
